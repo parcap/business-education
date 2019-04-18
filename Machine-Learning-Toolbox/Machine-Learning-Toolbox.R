51a93901cafab11be0788913f1f65876879ffa0c
nickjathar <jathar@parcapital.com>
2019-04-18 16:12:27 -0400

    updated content

--- Machine-Learning-Toolbox/Machine-Learning-Toolbox.R
+++ Machine-Learning-Toolbox/Machine-Learning-Toolbox.R
@@ -1,43 +1,25 @@
-setwd("C:/Users/njathar/Desktop/parcap-github/business-education/Machine-Learning-Toolbox")
-
 library(readxl)
 library(writexl)
-library(caret)
 
-donors <- read.csv("donors.csv")
+setwd("C:/Users/njathar/Desktop/parcap-github/business-education/Machine-Learning-Toolbox")
 
-table(donors$donated)
+stock_data <- read_xlsx("StockData.xlsx")
 
-donation_model <- glm(donated ~ bad_address + interest_religion + interest_veterans,
-                      data = donors,
-                      family = "binomial")
+months <- 12
 
-summary(donation_model)
+train <- stock_data[2:(months + 1), ]
+test <- stock_data[14:25, ]
 
-donors$donation_prob <- predict(donation_model, type = "response")
+stock_model <- lm(StockRet ~ BenchRet, train)
 
-donors$donation_pred <- ifelse(donors$donation_prob > mean(donors$donated), 1, 0)
+stock_predict <- predict(stock_model, test)
 
-mean(donors$donated == donors$donation_pred)
+stock_model_error <- stock_predict - test$StockRet
 
+stock_model_os_rmse <- sqrt(mean(stock_model_error ^ 2))
 
-# stocks <- read_xlsx("AMD_data.xlsx")
-# stocks <- stocks[!is.na(stocks$Return_1SF), c(5, 7)]
-# 
-# set.seed(42)
-# 
-# obs <- nrow(stocks)
-# stocks <- stocks[sample(obs), ]
-# 
-# stocks$Great_Return <- as.factor(stocks$Great_Return)
-# 
-# split <- round(obs * 0.6)
-# 
-# stocks_train <- stocks[1:split, ]
-# stocks_test <- stocks[(split + 1):obs, ]
-# 
-# model <- glm(Great_Return ~ ., family = "binomial", stocks_train)
-# prediction <- predict(model, stocks_test, type = "response")
-# prediction_class <- ifelse(prediction > 0.5, 1, 0)
-# prediction_cf <- factor(prediction_class, levels = levels(stocks_test$Great_Return))
-# confusionMatrix(prediction_cf, stocks_test$Great_Return)
+stock_is_predict <- predict(stock_model, train)
+
+stock_model_is_error <- stock_is_predict - train$StockRet
+
+stock_model_is_rmse <- sqrt(mean(stock_model_is_error ^ 2))
