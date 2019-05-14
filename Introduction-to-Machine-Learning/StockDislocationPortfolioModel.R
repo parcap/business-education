243ac9966202dda6839822495e9b1813aa6b32ee
nickjathar <jathar@parcapital.com>
2019-05-14 15:00:09 -0400

    Added content

---
+++ Introduction-to-Machine-Learning/StockDislocationPortfolioModel.R
@@ -0,0 +1,9 @@
+setwd("C:/Users/njathar/Desktop/parcap-github/business-education/Introduction-to-Machine-Learning")
+
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+library(readxl)
+
+sdp <- read_xlsx("StockDislocationPortfolioModel.xlsx")
