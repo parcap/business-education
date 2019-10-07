791f35798e22611ddcb0a26699532007410897d3
nickjathar <jathar@parcapital.com>
2019-10-06 21:19:00 -0400

    tweak

--- Introduction-to-Portfolio-Construction-and-Analysis-with-Python/max-drawdown.py
+++ Introduction-to-Portfolio-Construction-and-Analysis-with-Python/max-drawdown.py
@@ -1,12 +1,39 @@
 import pandas as pd
 
-me_m = pd.read_csv("data/Portfolios_Formed_on_ME_monthly_EW.csv",
+def drawdown(return_series: pd.Series):
+    """
+    Takes a time series of asset returns
+    Computes and returns a DataFrame that contains:
+        The wealth index
+        The previous peaks
+        The precent drawdowns
+    """
+    
+    wealth_index = 100 * (1 + return_series).cumprod()
+    previous_peaks = wealth_index.cummax()
+    drawdowns = (wealth_index - previous_peaks) / previous_peaks
+    
+    return pd.DataFrame({
+            "Wealth": wealth_index,
+            "Peaks": previous_peaks,
+            "Drawdown": drawdowns
+    })
+
+
+me_m = pd.read_csv('data/Portfolios_Formed_on_ME_monthly_EW.csv',
                    header = 0, index_col = 0, parse_dates = True, na_values = -99.99)
 
 rets = me_m[['Lo 10', 'Hi 10']]
 rets.columns = ['SmallCap', 'LargeCap']
 rets = rets / 100
-rets.plot.line()
 
+rets.index = pd.to_datetime(rets.index, format = '%Y%m').to_period('M')
 
+# rets['1975']
 
+drawdown(rets['LargeCap'])
+drawdown(rets['LargeCap'])[['Wealth', 'Peaks']].plot()
+drawdown(rets[:'1950']['LargeCap'])[['Wealth', 'Peaks']].plot()
+
+drawdown(rets['LargeCap'])['Drawdown'].min()
+drawdown(rets['LargeCap'])['Drawdown'].idxmin()
