10e8067c31bab3d462110a38f15149945b5a5e78
nickjathar <jathar@parcapital.com>
2019-10-23 19:54:51 -0400

    Update week3.py

--- Python-for-Data-Science-and-AI/week3.py
+++ Python-for-Data-Science-and-AI/week3.py
@@ -74,15 +74,84 @@ while(dates[i] != 1973):
 print(year)
 
 
-Function
+Functions
 
-def add(a):
+def add(a = 1):
     b = a + 1
     print(a, 'if you add one', b)
     return b
+    
+If there is no return statement, the function returns None.
+Note in the function add, the argument has a default value of 1
+
+
+# The function getBandRating below is good for understanding variable scope
+def getBandRating(bandname):
+    myFavouriteBand = "AC/DC"
+    if bandname == myFavouriteBand:
+        return 10.0
+    else:
+        return 0.0
+
+print("AC/DC's rating is: ", getBandRating("AC/DC"))
+print("Deep Purple's rating is: ", getBandRating("Deep Purple"))
+print("My favourite band is", myFavouriteBand)
+
+def con(a, b):
+    return(a + b)
+
+# Observe the output of various invocations of the function con -->
+con(1, 2)
+con("A", "B")
+con("A", 1)
+con(["A", 1], ["B", 2])
+
+
+
+Objects and Classes
+
+Every object has 1) a type, 2) an internal data representation (a blueprint),
+    and 3) a set of procedures for interacting with the object (methods)
+
+An object is an instance of a particular type
+
+type()   <-- used to find the type of an object
+
+A class or type's methods are functions that every instance of that class
+  or type provides. Methods are a means for interacting with the data in
+  an object.
+  
+A Class has data attributes and methods
+
+class Circle(object):
+    
+    def __init__(self, radius, color):
+        self.radius = radius;
+        self.color = color;
+        
+    def add_radius(self, r):
+        self.radius = self.radius + r
+        
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()  
+       
+once a class is defined, you can create (instantiate) objects of the class type
+  for example, you can create an object of type Circle
+
+C1 = Circle(4, "yellow")
+
+print(C1.color)  
+print(C1.radius) 
+
+C1.color = 'red'   # this changes the color attribute of the C1 instance
+
+print(C1.color)
+
+C1.add_radius(4)
+
+print(C1.radius)
+ 
+ dir(Circle)
 '''
-
-def add(a):
-    b = a + 1
-    print(a, 'if you add one', b)
-    return b
